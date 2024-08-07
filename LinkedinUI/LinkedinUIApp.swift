35cb9466ab279ca182dcd829288c964c0598949c
Anup Saud <anupsaud10@gmail.com>
2024-08-07 08:13:38 -0400

    Initial Commit

---
+++ LinkedinUI/LinkedinUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinUIApp.swift
+//  LinkedinUI
+//
+//  Created by Anup Saud on 2024-08-07.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
